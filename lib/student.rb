b559ebc377eadf006973b6801799e1158c6f6392
Iris <iris.thuo@student.moringaschool.com>
2023-02-19 08:17:26 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
 class Student 
-
+    require_relative './user'
+    class Student < User
+        attr_reader :knowledge
+        def initialize
+            @knowledge = []
+        end
+    
+        def learn(new_info)
+            @knowledge << new_info
+        end
 end
